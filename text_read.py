51f8cb126f65cc01adbe0ad6e7cb118f1a89a083
Cyril GUIET <cyril.guiet@atos.net>
2020-05-14 18:53:36 +0200

    initial commit

---
+++ text_read.py
@@ -0,0 +1,14 @@
+import cv2
+import pytesseract
+import argparse
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-p", "--path", type=str,
+	help="path to input image")
+args = vars(ap.parse_args())
+
+#pytesseract.pytesseract.tesseract_cmd = args["path"]
+
+img = cv2.imread(args["path"])
+text = pytesseract.image_to_string(img)
+print("Result: " + text)
